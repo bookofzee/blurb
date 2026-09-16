47fdc1859f8ae856d4491c6231dfa6493f3b5d6a
bookofzee <amywalford26@gmail.com>
2026-09-16 19:58:43 +0100

    Add Blurb service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='blurb-v5';
+const ASSETS=['/','/index.html','/styles.css?v=5','/app.js?v=5','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));});
